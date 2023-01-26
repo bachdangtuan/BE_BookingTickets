b46d535ec1fd7b1d57442f40af5d7a69e226f2d4
DΛn9Tuaz <85827570+bachdangtuan@users.noreply.github.com>
2023-01-27 02:33:09 +0700

    fix lại nào

--- models/trips.js
+++ models/trips.js
@@ -10,9 +10,9 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
 
-        static associate({Stations, passengerCarCompanies, Users}) {
+        static associate({Stations, passengerCarCompanies, users}) {
             // Quan hệ chuyến đi và người, quan hệ nhiều nhiều
-            this.belongsToMany(Users, {through: 'Tickets', as: 'user', foreignKey: 'trip_id',});
+            this.belongsToMany(users, {through: 'Tickets', as: 'user', foreignKey: 'trip_id',});
             //Quan hệ giữa chuyến đi và nhà xe,quan hệ nhiều nhiều
             this.belongsToMany(passengerCarCompanies, {through: 'Route', foreignKey: 'trip_id', as: 'company'});
             //Quan hệ giữa chuyến đi và bến xe điểm đi (1-n)
